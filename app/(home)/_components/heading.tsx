9309b3a41b2b61ee9be07dd9e2d31c25baace280
murtazaisback <muhammadmurtazanasir90@gmail.com>
2023-10-31 12:28:10 +0300

    fixMore

--- app/(home)/_components/heading.tsx
+++ app/(home)/_components/heading.tsx
@@ -17,7 +17,7 @@ export const Heading = () => {
         Your Ideas, Documents, & Plans. Unified. Welcome To <span className="underline">NEKOLA</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br />
+        NEKOLA is the connected workspace where <br />
         better, faster work happens.
       </h3>
       {isLoading && (
